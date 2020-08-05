e8d1f0bf7e4e31b5413323379914fb22e8b88382
GMTraid <danilworkpage69@gmail.com>
2020-08-05 14:22:59 +0300

    Clarification in the project description

--- src/AlgebraicOperations/matrixOperations.h
+++ src/AlgebraicOperations/matrixOperations.h
@@ -1,4 +1,9 @@
+#include "src/primitiveTypes"
+
 #ifndef MATHEMATICALMODEL_MATRIXOPERATIONS_H
 #define MATHEMATICALMODEL_MATRIXOPERATIONS_H
+
+void adjoint_matrix(int array d2_array, )
+
 
 #endif //MATHEMATICALMODEL_MATRIXOPERATIONS_H
